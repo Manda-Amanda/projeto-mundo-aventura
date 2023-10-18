ef8c955ebd2c4421291d677f8144378cf363e3e0
rickOmig <oliveiraenrico1@gmail.com>
2023-10-18 10:43:41 -0300

    Criação do Formulário

---
+++ mundoaventura/src/main/java/br/com/mundoaventura/servlet/CreateMundoAventura.java
@@ -0,0 +1,24 @@
+package br.com.mundoaventura.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/CreateLocal")
+public class CreateLocalServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String carName = request.getParameter("localname");
+
+        System.out.println(localname);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+
+    }
+
+}
